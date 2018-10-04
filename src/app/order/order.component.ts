3f05136052b7def21b9603a474f0b0c152704cd9
Thiago Coppi <thiago.coppi@senior.com.br>
2018-10-04 08:27:45 -0300

    Até a aula 73

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,7 +4,7 @@ import { RadioOptions } from './../shared/radio/radio-options.model';
 import { Component, OnInit } from '@angular/core';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mt-order',
@@ -13,6 +13,8 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class OrderComponent implements OnInit {
 
   orderForm: FormGroup;
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  numberPattern = /^[0-9]*$/;
 
   paymentOptions : RadioOptions[] = [
     {label: 'Cartão', value : 'CAR'},
@@ -58,13 +60,13 @@ export class OrderComponent implements OnInit {
 
   ngOnInit() {
     this.orderForm = this.formBuilder.group({
-      name: this.formBuilder.control(''),
-      email: this.formBuilder.control(''),
-      emailConfirmation: this.formBuilder.control(''),
-      address: this.formBuilder.control(''),
-      number: this.formBuilder.control(''),
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
       complemento: this.formBuilder.control(''),
-      formasPagamentos: this.formBuilder.control('')
+      formasPagamentos: this.formBuilder.control('', [Validators.required])
     });
   }
 
